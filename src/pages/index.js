b22a4ffbde605b8614d1fc8e087eedcce5bedc48
Anubhav Srivastava <anubhav.srivastava00@gmail.com>
2019-07-30 00:21:57 +0530

    background change

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,15 +15,54 @@ const IndexPage = () => (
   <Layout>
     <section id="banner">
       <div className="inner">
-        <div className="logo">
-          <span className="icon fa-diamond"></span>
-        </div>
         <h2>{config.heading}</h2>
         <p>{config.subHeading}</p>
       </div>
     </section>
 
     <section id="wrapper">
+      <section id="one" className="wrapper spotlight style1">
+        <div className="inner">
+          <a href="/#" className="image">
+            <img src={pic1} alt="" />
+          </a>
+          <div className="content">
+            <h2 className="major">Magna arcu feugiat</h2>
+            <p>
+              Lorem ipsum dolor sit amet, etiam lorem adipiscing elit. Cras
+              turpis ante, nullam sit amet turpis non, sollicitudin posuere
+              urna. Mauris id tellus arcu. Nunc vehicula id nulla dignissim
+              dapibus. Nullam ultrices, neque et faucibus viverra, ex nulla
+              cursus.
+            </p>
+            <a href="/#" className="special">
+              Learn more
+            </a>
+          </div>
+        </div>
+      </section>
+
+      <section id="two" className="wrapper alt spotlight style2">
+        <div className="inner">
+          <a href="/#" className="image">
+            <img src={pic2} alt="" />
+          </a>
+          <div className="content">
+            <h2 className="major">Tempus adipiscing</h2>
+            <p>
+              Lorem ipsum dolor sit amet, etiam lorem adipiscing elit. Cras
+              turpis ante, nullam sit amet turpis non, sollicitudin posuere
+              urna. Mauris id tellus arcu. Nunc vehicula id nulla dignissim
+              dapibus. Nullam ultrices, neque et faucibus viverra, ex nulla
+              cursus.
+            </p>
+            <a href="/#" className="special">
+              Learn more
+            </a>
+          </div>
+        </div>
+      </section>
+
       <section id="one" className="wrapper spotlight style1">
         <div className="inner">
           <a href="/#" className="image">
