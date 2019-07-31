a0b400e35819e61fd839d28c5e534696de96ba98
Anubhav Srivastava <anubhav.srivastava00@gmail.com>
2019-07-31 23:00:07 +0530

    added projecr json

---
+++ src/data/projects.js
@@ -0,0 +1,18 @@
+export default [
+  {
+    name: "What's my HTTP Status Code",
+    link: 'https://httpstatuscode.netlify.com',
+    logoName: 'httpcode.svg',
+    description:
+      'This projects helps you choose appropriate HTTP status code for your web APIs. Based on series of questions, it lets arrive to a appropriate HTTP status code (with reason). In short, it makes choosing HTTP code much easier.',
+    github: 'https://github.com/anubhavsrivastava/whats-my-http-statuscode',
+  },
+  {
+    name: 'Perceptible',
+    link: 'https://perceptible.netlify.com',
+    logoName: 'perceptible.png',
+    description:
+      'Perceptible can be used to detect viewability of any DOM element that the user is currently looking at. A Element may be part of the page but not under current viewport (due to user scroll), perceptible helps to calculate viewability for such elements for analytics and other purpose.',
+    github: 'https://github.com/anubhavsrivastava/perceptible',
+  },
+];
